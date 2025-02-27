f0a8376468df77537fa54bb200acc7a7f444c538
clinton yade <yadeclinton20@gmail.com>
2025-02-27 13:19:51 +0300

    Added DataFrame creation in Pandas script

--- pandas_script.py
+++ pandas_script.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+
+# 1. Creating DataFrames
+print("### Creating DataFrames ###")
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'Salary': [50000, 60000, 70000]}
+df = pd.DataFrame(data)
+print(df, '\n')
